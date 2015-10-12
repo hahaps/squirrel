524272e80fd4a5af9f486aae8c475f4b252b0aa4
Li Xipeng <lixipeng@hihuron.com>
2015-10-12 18:58:38 +0800

    Initial squirrel

---
+++ test/connection.js
@@ -0,0 +1,13 @@
+var Connection = require('../lib/connection')
+, assert = require('assert');
+
+describe('Connection', function(){
+  it('should return a object with connection methods', function() {
+    var connection = new Connection('www.test.com', 'test');
+    assert(connection instanceof Connection);
+
+    // compare baseURI and prefix
+    assert(connection.baseURI == 'www.test.com');
+    assert(connection.prefix == 'test');
+  });
+});
